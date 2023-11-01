61dce42385fb230631d6d8088672b6448f9f125e
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-11-01 23:47:50 +0530

    Removed the Contact Me Text for Better Attention to Sub-Texts (Experiment)

--- src/components/contact-me/Contact.jsx
+++ src/components/contact-me/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
             <div className="flex flex-row justify-center items-center gap-5">
                 <div className="basis-6/12">
                     <div className="text-left">
-                        <h1 className="text-7xl font-bold">Contact Me</h1>
-                        <h4 className="text-lg pt-5 text-gray-400">I am currently available for freelance opportunities or any exciting offers that may align with my skills and expertise. You can reach me via phone at <a href="tel:+918421045354" className="hover:text-red-600 text-white font-semibold">+918421045354</a> or by email at <a href="mailto:samnayakawadi@gmail.com" className="hover:text-red-600 text-white font-semibold">samnayakawadi@gmail.com</a>. Alternatively, you may use the provided contact form to send a direct email. I look forward to connecting with you.</h4>
+                        {/* <h1 className="text-7xl font-bold">Contact Me</h1> */}
+                        <h4 className="text-lg text-gray-400">I am currently available for freelance opportunities or any exciting offers that may align with my skills and expertise. You can reach me via phone at <a href="tel:+918421045354" className="hover:text-red-600 text-white font-semibold">+918421045354</a> or by email at <a href="mailto:samnayakawadi@gmail.com" className="hover:text-red-600 text-white font-semibold">samnayakawadi@gmail.com</a>. Alternatively, you may use the provided contact form to send a direct email. I look forward to connecting with you.</h4>
                         {/* <h4 className="text-lg px-52 text-gray-400">Connect with me via phone <a href="tel:+918421045354" className="hover:text-red-600">+918421045354</a> or email <a href="mailto:samnayakawadi@gmail.com" className="hover:text-red-600">samnayakawadi@gmail.com</a></h4> */}
                         {/* <h4 className="text-lg px-52 text-gray-400">Or Send the direct email using following contact form.</h4> */}
                     </div>
