047aee6495e4b80749cd8740bc7ba05dfa4cd9c6
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-06-15 20:02:21 +0530

    Updated the Contact Page Colour + Experience

--- src/components/about-me/selector/Experience.jsx
+++ src/components/about-me/selector/Experience.jsx
@@ -3,7 +3,7 @@ const Experience = () => {
         <div className="flex flex-col gap-3 text-lg">
             <div>
                 <h1 className="text-gray-400 font-bold">Project Engineer - Centre for Development of Advanced Computing</h1>
-                <p className="text-gray-500 pt-2 font-bold">1 Year, 5 Months</p>
+                <p className="text-gray-500 pt-2 font-bold">1 Year, 10 Months</p>
 
                 <p className="text-gray-500 pt-2">During my tenure at CDAC, I spearheaded the research on the ideal database for a new project. Taking charge of the MeghSikshak Assessment project as the sole developer, I meticulously designed JSON structures for MongoDB collections.</p>
                 <p className="text-gray-500 pt-2">Crafting modern user interfaces across three role dashboards and implementing the backend with Spring Boot & Node JS, I assumed full responsibility for project success. Concurrently, I served as a guide for CDAC{"'"}s DAC Course Students, overseeing two batches.</p>
